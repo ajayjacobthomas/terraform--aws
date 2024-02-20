555041be812fb5a0819a5c38e95774c434d1e757
Ajay Thomas <ajgcplearning@gmail.com>
2024-02-20 21:31:09 +0000

    updated eip file

--- eip.tf
+++ eip.tf
@@ -2,7 +2,11 @@
 # GitHub    : https://github.com/ajayjacobthomas/terraform-aws
 ########################################################
 
-resource "aws_eip" "myeip" {
-  //instance = aws_instance.web.id
-  vpc      = true
+# Create AWS VPC in eu-west-1
+# CIDR - 10.0.0.0/16
+resource "aws_vpc" "vpc-eu-west-1" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "VPC: set-on-eu-west-1"
+  }
 }
