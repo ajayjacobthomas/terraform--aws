b35d6a1b301668e4e25cc1dbccd4d7ce64b8f8d3
Ajay Thomas <ajayjacobthomas@gmail.com>
2024-04-04 17:24:51 +0100

    final working commit

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2_example" {
-  ami           = "ami-080e1f13689e07408"
+  ami           = "ami-0f007bf1d5c770c6e"
   instance_type = "t2.micro"
   tags = {
     Name = "Terraform EC2"
