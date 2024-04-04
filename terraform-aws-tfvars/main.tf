ddf948c82965a9eb5dc19a586dbbc9e5b63cbba7
Ajay Thomas <ajayjacobthomas@gmail.com>
2024-04-04 18:45:56 +0100

    updated main and variables.

--- terraform-aws-tfvars/main.tf
+++ terraform-aws-tfvars/main.tf
@@ -1,7 +1,13 @@
 resource "aws_instance" "first_ec2_Ireland" {
-  ami           = "ami-0f007bf1d5c770c6e"
-  instance_type = "var.instance_type"
-  tags = {
-    Name = "Terraform EC2"
-  }
-} 
+  ami                         = "ami-0f007bf1d5c770c6e"
+  instance_type               = var.instance_type
+  count                       = var.instance_count
+  associate_public_ip_address = var.enable_public_ip
+  tags                        = var.project_environment
+}
+
+# Create IAM users
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
--- terraform-aws-tfvars/variables.tf
+++ terraform-aws-tfvars/variables.tf
@@ -1,13 +1,17 @@
 # variable.tf 
 
+#String variable type
 variable "instance_type" {
    type = string
    description = "EC2 Instance Trials"
+   default = "t2.micro"
 }
 
-variable "tag" {
-   type = string
-   description = "Instance created by terraform"
+# Number variable type
+variable "instance_count" {
+  description = "EC2 instance count"
+  type        = number
+  default     = 2
 }
 
 # default value for the variable location
@@ -15,4 +19,28 @@ variable "location" {
    type = string
    description = "The project region"
    default = "eu-west-1"
+}
+
+# Boolean variable type
+variable "enable_public_ip" {
+  description = "Enable public IP address"
+  type        = bool
+  default     = true
+}
+
+# List variable types
+variable "user_names" {
+  description = "IAM usernames"
+  type        = list(string)
+  default     = ["vscode1", "vscode2", "vscode3"]
+}
+
+# Map variable type
+variable "project_environment" {
+  description = "project name and environment"
+  type        = map(string)
+  default     = {
+    project     = "project-delta",
+    environment = "dev"
+  }
 }
