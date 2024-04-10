367474477196ee1c525f0b0cb63ce93eed6c49af
Ajay Thomas <ajayjacobthomas@gmail.com>
2024-04-10 15:45:31 +0100

    output examples

--- terraform-aws-local/main.tf
+++ terraform-aws-local/main.tf
@@ -7,7 +7,7 @@ locals {
 
 # Define VPC - note the local variable defined
 resource "aws_vpc" "custom-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
---
+++ terraform-output-values/main.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "ec2_example" {
+  ami           = "ami-0f007bf1d5c770c6e"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Terraform EC2 instance for output example"
+  }
+}
