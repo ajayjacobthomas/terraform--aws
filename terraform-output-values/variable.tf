3013e6658dd137c0b11886f733296c857713c7ff
Ajay Thomas <ajayjacobthomas@gmail.com>
2024-04-10 15:54:19 +0100

    updated tfvars and variables

--- terraform-output-values/variable.tf
+++ terraform-output-values/variable.tf
@@ -1,11 +1,9 @@
 variable "aws_region" {
   description = "The AWS region to create resources in."
   type        = string
-  default     = "eu-west-1"
 }
 
 variable "instance_type" {
   description = "The instance type of the EC2 instance."
   type        = string
-  default     = "t2.micro"
 }
